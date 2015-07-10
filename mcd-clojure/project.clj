4e29628520e406280cf2f06809ac189ff640005a
Gianluca Ortelli <gianluca.ortelli@trifork.nl>
2015-07-10 12:52:09 +0200

    Follow lein's directory layout.

--- mcd-clojure/project.clj
+++ mcd-clojure/project.clj
@@ -1,4 +1,4 @@
-(defproject mcd/exercises "0.0.1"
+(defproject mcd "0.0.1"
   :description "Modern Compiler Design exercises"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
